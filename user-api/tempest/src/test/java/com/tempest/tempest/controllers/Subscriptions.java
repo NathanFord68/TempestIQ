7f74336a4666d0e76c56806cdd9b4a331ca36e32
= <=>
2026-05-26 19:07:52 -0400

    Updated tests to use the Mongo mock client

--- user-api/tempest/src/test/java/com/tempest/tempest/controllers/Subscriptions.java
+++ user-api/tempest/src/test/java/com/tempest/tempest/controllers/Subscriptions.java
@@ -1,7 +1,14 @@
 package com.tempest.tempest.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.powermock.api.mockito.PowerMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +19,7 @@ import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import com.tempest.tempest.Entities.Subscription;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,28 +33,61 @@ class SubscriptionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    Subscription subscriptionCreateObject = new Subscription(
-        "nathforl",
-        "US", 
-        "Tenessee", 
-        "Kingsport", 
-        70.00f, 
-        80.0f
-    );
 
-    Subscription subscriptionUpdateObject = new Subscription(
-        "123456789",
-        "nathforl",
-        "US", 
-        "Tenessee", 
-        "Kingsport", 
-        80.00f, 
-        90.0f
-    );
-
+    // Mock database variables
+    MongoCollection<Subscription> dbCollection;
+    MongoClient mongo;
+    MongoDatabase db;
     
+    @BeforeEach
+    @SuppressWarnings("unchecked")
+    void resetDatabase() {
+        dbCollection = PowerMockito.mock(MongoCollection.class);
+        mongo = PowerMockito.mock(MongoClient.class);
+        db = PowerMockito.mock(MongoDatabase.class);
+
+        List<Subscription> mockSubscriptions = new ArrayList<Subscription>();
+
+        mockSubscriptions.add(new Subscription(
+            "jasonborn",
+            "US", 
+            "Tenessee", 
+            "Kingsport", 
+            10.00f, 
+            20.0f
+        ));
+        mockSubscriptions.add(new Subscription(
+            "jasonborn",
+            "US", 
+            "Virginia", 
+            "Ashburn", 
+            30.00f, 
+            40.0f
+        ));
+        mockSubscriptions.add(new Subscription(
+            "jasonborn",
+            "US", 
+            "Arizona", 
+            "Phoenix", 
+            70.00f, 
+            80.0f
+        ));
+
+        dbCollection.insertMany(mockSubscriptions);
+    }
+
     @Test
     void shouldCreateSubscription() throws Exception {
+
+        Subscription subscriptionCreateObject = new Subscription(
+            "nathforl",
+            "US", 
+            "Tenessee", 
+            "Kingsport", 
+            70.00f, 
+            80.0f
+        );
+        
         mockMvcTester.post()
             .uri("/subscriptions")
             .contentType(MediaType.APPLICATION_JSON)
@@ -81,6 +122,15 @@ class SubscriptionControllerTest {
     @Test
     void shouldReturnSubscriptionById() throws Exception {
 
+        Subscription subscriptionCreateObject = new Subscription(
+            "nathforl",
+            "US", 
+            "Tenessee", 
+            "Kingsport", 
+            70.00f, 
+            80.0f
+        );
+
         var createResponse = mockMvcTester.post()
             .uri("/subscriptions")
             .contentType(MediaType.APPLICATION_JSON)
@@ -107,6 +157,25 @@ class SubscriptionControllerTest {
     @Test
     void shouldUpdateSubscription() throws Exception {
 
+        Subscription subscriptionCreateObject = new Subscription(
+            "nathforl",
+            "US", 
+            "Tenessee", 
+            "Kingsport", 
+            70.00f, 
+            80.0f
+        );
+
+        Subscription subscriptionUpdateObject = new Subscription(
+            "123456789",
+            "nathforl",
+            "US", 
+            "Tenessee", 
+            "Kingsport", 
+            80.00f, 
+            90.0f
+        );
+        
         var createResponse = mockMvcTester.post()
             .uri("/subscriptions")
             .contentType(MediaType.APPLICATION_JSON)
